250330433e254046b51e12943d535a60e9c01ac8
Parth442002 <parthkatiyar04@gmail.com>
2022-04-29 19:00:09 +0530

    added Pre-commit Functionality

--- server/server/settings.py
+++ server/server/settings.py
@@ -195,7 +195,7 @@ JWT_AUTH_REFRESH_COOKIE = 'refresh_token'
 
 # SIMPLE_JWT TOKEN
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
